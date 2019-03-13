4b08f24e374c5a5f865f084e6d432a8021c672d0
tony <334650420@qq.com>
2019-03-13 21:54:20 +0800

    20190313学习整理阶段

---
+++ run.py
@@ -0,0 +1,35 @@
+from HardInfoWin import *
+import re
+import platform
+
+
+class run(object):
+
+    def __init__(self):
+        ostype = platform.system()
+        print('操作系统类型：' + ostype)
+        self.hardinfo = HardInfoWin()
+
+    def select():
+        count = 1
+        while count == 1:
+            print('____________________')
+            print('服务器系统运维脚本：')
+            print('——1、服务器硬件信息巡检')
+            print('——2、操作系统信息巡检')
+            print('——3、中间件系统巡检')
+            print('——4、数据库系统巡检')
+            print('——5、信息系统巡检')
+            sel01 = input('请选择巡检类型(数字键1-5，0退出)：')
+
+            while not re.findall('^[0-5]+$', sel01):
+                # 限定只能输入数字
+                sel01 = input("选择错误！请选择数字键0-5")
+                print('选择6项：' + sel01)
+            if str(sel01) == '1':
+                hardinfo.printCPU()
+            if str(sel01) == '0':
+                break
+
+go = run()
+run.select()
